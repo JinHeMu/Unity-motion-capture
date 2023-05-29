28f51751e0fcb01018a09409956c72ad111f0638
JinHeMu <490754775@qq.com>
2023-05-29 13:14:09 +0800

    2023 /5/29

--- 1064/E05_04_imu660ra_demo/code/get_value.c
+++ 1064/E05_04_imu660ra_demo/code/get_value.c
@@ -3,14 +3,21 @@
 #define M_PI 3.1415926f
 
 Angle head;Raw_Data head_Raw;
-//Angle head;Raw_Data head_Raw;
-//Angle head;Raw_Data head_Raw;
-//Angle head;Raw_Data head_Raw;
-//Angle head;Raw_Data head_Raw;
-//Angle head;Raw_Data head_Raw;
-//Angle head;Raw_Data head_Raw;
-//Angle head;Raw_Data head_Raw;
-//Angle head;Raw_Data head_Raw;
+
+Angle spine;Raw_Data spine_Raw;
+
+Angle thigh_l;Raw_Data thigh_l_Raw;
+Angle thigh_r;Raw_Data thigh_r_Raw;
+
+Angle calf_l;Raw_Data calf_l_Raw;
+Angle calf_r;Raw_Data calf_r_Raw;
+
+Angle upperarm_l;Raw_Data upperarm_l_Raw;
+Angle upperarm_r;Raw_Data upperarm_r_Raw;
+
+Angle lowerarm_l;Raw_Data lowerarm_l_Raw;
+Angle lowerarm_r;Raw_Data lowerarm_r_Raw;
+
 
 
 void Value_process(float imu660ra_gyro_x, float imu660ra_gyro_y, float imu660ra_gyro_z, Angle *Object, Raw_Data *Object_raw)
@@ -40,7 +47,7 @@ void Value_process(float imu660ra_gyro_x, float imu660ra_gyro_y, float imu660ra_
 	Object->Angle_Z += (Object_raw->gyro_z / 3.33);
 	Object->Angle_X_F = Kalmen_getAngle1(Object->Angle_X, Object_raw->gyro_x, 0.005);
 	Object->Angle_Y_F = Kalmen_getAngle2(Object->Angle_Y, Object_raw->gyro_y, 0.005);
-	Object->Angle_Z_F = Kalmen_getAngle3(Object->Angle_Z, Object_raw->gyro_z, 0.005);
+//	Object->Angle_Z_F = Kalmen_getAngle3(Object->Angle_Z, Object_raw->gyro_z, 0.005);
 
 
 
@@ -62,7 +69,12 @@ void Get_Value(void)
 //  imu660ra_get_gyro_1();                                                        // 获取 IMU660RA 的角速度测量数值
 //	Value_process(imu660ra_gyro_x_1, imu660ra_gyro_y_1, imu660ra_gyro_z_1, &head, &head_Raw);
 	
-	imu660ra_get_acc_9();                                                         // 获取 IMU660RA 的加速度测量数值
-  imu660ra_get_gyro_9();                                                        // 获取 IMU660RA 的角速度测量数值
+	imu660ra_get_acc_9();                                                         
+  imu660ra_get_gyro_9();
+	
+	imu963ra_get_gyro();
+	imu963ra_get_acc();
+
 	Value_process(imu660ra_gyro_x_9, imu660ra_gyro_y_9, imu660ra_gyro_z_9, &head, &head_Raw);
+	Value_process(imu963ra_gyro_x, imu963ra_gyro_y, imu963ra_gyro_z, &upperarm_l, &upperarm_l_Raw);
 }
--- 1064/E05_04_imu660ra_demo/code/get_value.h
+++ 1064/E05_04_imu660ra_demo/code/get_value.h
@@ -21,8 +21,22 @@ typedef struct {
     float Angle_Z_F;
 } Angle;
 
-extern Angle head;
-extern Raw_Data head_Raw;
+
+extern Angle head;extern Raw_Data head_Raw;
+
+extern Angle spine;extern Raw_Data spine_Raw;
+
+extern Angle thigh_l;extern Raw_Data thigh_l_Raw;
+extern Angle thigh_r;extern Raw_Data thigh_r_Raw;
+
+extern Angle calf_l;extern Raw_Data calf_l_Raw;
+extern Angle calf_r;extern Raw_Data calf_r_Raw;
+
+extern Angle upperarm_l;extern Raw_Data upperarm_l_Raw;
+extern Angle upperarm_r;extern Raw_Data upperarm_r_Raw;
+
+extern Angle lowerarm_l;extern Raw_Data lowerarm_l_Raw;
+extern Angle lowerarm_r;extern Raw_Data lowerarm_r_Raw;
 
 void Get_Value(void);
 #endif
